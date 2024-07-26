45a83b66ed798480b0129849386b029ae4583b6d
Ravi Prakash <129740581+raviiai@users.noreply.github.com>
2024-07-26 20:51:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+
+Copy code
 terraform {
   required_providers {
     aws = {
@@ -7,6 +9,14 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "tf-remote-backend-backstage"  # Name of your S3 bucket
+    key            = "terraform.tfstate"            # The key within the bucket where the state file will be stored
+    region         = "ap-south-1"                  # AWS region of the bucket
+    encrypt        = true                           # Encrypt the state file
+    dynamodb_table = "terraform-lock-table"         # Optional: DynamoDB table for state locking and consistency checks
+  }
 }
 
 provider "aws" {
