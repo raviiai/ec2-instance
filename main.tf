24f53b2d605a5752e559984b316ea5fc5157fad6
Ravi Prakash <129740581+raviiai@users.noreply.github.com>
2024-08-05 17:34:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,24 +33,20 @@ resource "aws_instance" "example" {
   instance_type = "t2.small"
 
   # User data to install Docker, Docker Compose, pull from GitHub, and run Docker Compose
-  user_data = <<-EOF
-    #!/bin/bash
-    # Update package list and install Docker and Git
-    sudo apt-get update
-    sudo apt-get install -y docker.io git curl
-
-    # Start Docker and enable it to run on boot
-    sudo systemctl start docker
-    sudo systemctl enable docker
-
-    # Add the EC2 user to the Docker group
-    sudo usermod -aG docker ubuntu
-
-    # Clone the repository
-    git clone https://github.com/raviiai/docker-compose.git /home/ubuntu/docker-compose
-    sudo docker run -d -p 80:80 --name mynginx nginx
-
-  EOF
+ user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt update -y
+              sudo apt install docker-ce docker-ce-cli containerd.io -y
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              git clone https://github.com/raviiai/docker-compose.git
+              cd docker-compose
+              sudo docker compose up
+              EOF
 
   security_groups = [aws_security_group.allow_ssh_http.name]
 
@@ -58,6 +54,7 @@ resource "aws_instance" "example" {
     Name = "example-instance"
   }
 }
+
 
 output "instance_id" {
   value = aws_instance.example.id
